1a2e51498ed85b2ca4fd9d770ad0148045e27c8a
Robert W Boyer <rwboyer@mac.com>
2016-04-23 11:18:23 -0400

    full width page template

---
+++ template-parts/content-page-full.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package McCrery-Harra
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="featured-image">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+			<?php the_post_thumbnail(); ?>
+			</a>
+		<?php endif; ?>
+	</div>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'hooch' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php edit_post_link( esc_html__( 'Edit', 'hooch' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
---
+++ templates/template-nosidebar.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template Name: Nosidebar
+ *
+ *
+ * @package McCrery-Harra
+ */
+
+get_header(); ?>
+
+<?php 
+	 // Gets the uploaded featured image
+  	$featured_img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+  	// Checks and returns the featured image
+  	$bg = (!empty( $featured_img ) ? "background-image: url('". $featured_img[0] ."');" : '');
+?>
+
+<div class="wrap">
+	<div id="primary" class="content-home">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page-full' ); ?>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+</div> <!-- .wrap -->
+
+<?php get_footer(); ?>
